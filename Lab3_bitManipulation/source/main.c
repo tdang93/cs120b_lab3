2999e8003b928a3f76c68a938645fe429101be2d
Luccas Chang <lchan063@wch136-22.cs.ucr.edu>
2020-01-14 13:44:57 -0800

    main file written but untested

--- Lab3_bitManipulation/source/main.c
+++ Lab3_bitManipulation/source/main.c
@@ -18,7 +18,12 @@ int main(void) {
 	DDRC = 0x00; PORTC = 0x00;
     /* Insert your solution below */
 	while (1) {
-		if ( (PINA & 0x01) || (PINA &0x02)) {PINC | 0x60;}
+		if ( (PINA & 0x01) || (PINA & 0x02)) {PINC | 0x60;}
+		if ( (PINA & 0x03) || (PINA & 0x04)) {PINC | 0x70;}
+		if ( (PINA & 0x05) || (PINA & 0x06)) {PINC | 0x38;}
+		if ( (PINA & 0x07) || (PINA & 0x08) || (PINA & 0x09)) {PINC | 0x3C;}
+		if ( (PINA & 0x0A) || (PINA & 0x0B) || (PINA & 0x0C)) {PINC | 0x3E;}
+		if ( (PINA & 0x0D) || (PINA & 0x0E) || (PINA & 0x0F)) {PINC | 0x3F;}
 	}
 	return 1;
 }
