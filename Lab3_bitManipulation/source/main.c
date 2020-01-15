942cd56f9eed092d202a40e3614acfe6f1f973e8
Luccas Chang <lchan063@wch136-22.cs.ucr.edu>
2020-01-14 18:28:06 -0800

    copied over main file from exercise 2

--- Lab3_bitManipulation/source/main.c
+++ Lab3_bitManipulation/source/main.c
@@ -17,6 +17,15 @@ int main(void) {
 
     /* Insert your solution below */
     while (1) {
+	PORTC = 0x00;
+	
+	if ( (PINA == 0x01) || (PINA == 0x02)) {PORTC = 0x60;}
+	if ( (PINA == 0x03) || (PINA == 0x04)) {PORTC = 0x70;}
+	if ( (PINA == 0x05) || (PINA == 0x06)) {PORTC = 0x38;}
+	if ( (PINA == 0x07) || (PINA == 0x08) || (PINA == 0x09)) {PORTC = 0x3C;}
+	if ( (PINA == 0x0A) || (PINA == 0x0B) || (PINA == 0x0C)) {PORTC = 0x3E;}
+	if ( (PINA == 0x0D) || (PINA == 0x0E) || (PINA == 0x0F)) {PORTC = 0x3F;}
+	//(PORTC | 0x00) != (PORTC = PORTC | 0x00)
 
     }
     return 1;
